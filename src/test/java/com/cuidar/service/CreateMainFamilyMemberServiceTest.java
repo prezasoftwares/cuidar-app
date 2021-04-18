ee0817c182f019a725e288bd98f3058a52d2803a
Moacir Jr <prezasoftwares@gmail.com>
2021-04-18 19:53:36 -0300

    removing unused import reference

--- src/test/java/com/cuidar/service/CreateMainFamilyMemberServiceTest.java
+++ src/test/java/com/cuidar/service/CreateMainFamilyMemberServiceTest.java
@@ -1,7 +1,6 @@
 package com.cuidar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
