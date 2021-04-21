457f64cb9d7901b8b5a97ccea74270ad2842c1ee
Moacir Jr <prezasoftwares@gmail.com>
2021-04-21 13:29:05 -0300

    removing unecessary cross origin

--- src/main/java/com/cuidar/controller/MainFamilyMemberController.java
+++ src/main/java/com/cuidar/controller/MainFamilyMemberController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 @RequestMapping("mainfamilymembers")
 public class MainFamilyMemberController {
 
