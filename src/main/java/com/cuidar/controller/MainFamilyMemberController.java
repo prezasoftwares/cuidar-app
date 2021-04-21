29823d9b0e34646d65790d57e3d85217af6a7591
Moacir Jr <prezasoftwares@gmail.com>
2021-04-21 12:45:25 -0300

    cross origin added to controller

--- src/main/java/com/cuidar/controller/MainFamilyMemberController.java
+++ src/main/java/com/cuidar/controller/MainFamilyMemberController.java
@@ -12,6 +12,7 @@ import com.cuidar.service.FindMainFamilyMemberService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("mainfamilymembers")
 public class MainFamilyMemberController {
 
